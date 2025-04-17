f5aa76b7302f4c01b3818d24a9deb5af2813c598
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-17 11:29:27 -0700

    Completed Dynamically Render Data

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const Recipe = ({ }) => {
+
+const Recipe = ({ name, ingredients, instructions }) => {
   return (
-    
+    <div>
+      <h3>{name}</h3>
+
+
+      <h4>Ingredients:</h4>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+
+
+      <h4>Instructions:</h4>
+      <ol>
+        {instructions.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+    </div>
   );
 };
+
 
 export default Recipe;
--- src/RecipeContainer.jsx
+++ src/RecipeContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Recipe from './Recipe';
 
+
 const RecipeContainer = ({ recipes }) => {
   return (
     <div>
-     
+      <h2>Recipe List</h2>
+      {recipes.map((recipe, index) => (
+        <Recipe key={index} {...recipe} />
+      ))}
     </div>
   );
 };
