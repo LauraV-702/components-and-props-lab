9bea81929c8ba22410e0cbac649901d86c306094
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-17 11:26:45 -0700

    Completed Import and Pass Data Through Props

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import { recipes } from "../data.js";
 
 function App() {
   return (
-    
+    <div>
+      <h1>My Recipe App</h1>
+      <RecipeContainer recipes={recipes} />
+    </div>
   );
 }
+
 
 
 export default App;
